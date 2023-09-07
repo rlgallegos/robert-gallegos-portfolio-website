a2ce5b7588f86debf7dca038a7c041c703065dcf
Robert <rlgallegos85@gmail.com>
2023-09-07 15:45:01 -0400

    Finished speeding up load times

--- src/components/ProjectDetailsPopup.js
+++ src/components/ProjectDetailsPopup.js
@@ -1,5 +1,8 @@
+import { useState } from "react"
+
 
 function ProjectDetailsPopup({project, setPopup, setIsBlurry}){
+    const [isLoaded, setIsLoaded] = useState(false)
 
     function handleExit(e){
         if (!e.target.closest(".popup") || e.target.textContent === 'Exit') {
@@ -7,8 +10,10 @@ function ProjectDetailsPopup({project, setPopup, setIsBlurry}){
             setIsBlurry(false)
         }
     }
+    let uniqueID = 1000
     let descriptionParagraphs = project.description.map(paragraph => {
-        return <p className="text-justify md:mx-8 my-2 indent-5">{paragraph}</p>
+        uniqueID++
+        return <p key={uniqueID} className="text-justify md:mx-8 my-2 indent-5">{paragraph}</p>
     })
 
     return (
@@ -16,8 +21,17 @@ function ProjectDetailsPopup({project, setPopup, setIsBlurry}){
             <div className="popup p-4 w-full md:w-3/4 h-[75vh] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 bg-white fixed z-50 border border-slate-800 overflow-scroll">
                 <div className="p-2 lg:p-4">
                     <h2 className="text-3xl font-bold my-4">{project.title}</h2>
-                    <div className="border-2 border-slate-800 rounded-lg ring-2 ring-offset-2 ring-slate-800 p-4 my-4 flex flex-col">
-                        <video src={project.video} autoPlay loop muted playsInline loading='lazy' ></video>
+                    <div className="border-2 border-slate-800 rounded-lg ring-2 ring-offset-2 ring-slate-800 p-4 my-4 flex flex-col relative">
+                    {!isLoaded && <p className="mx-auto text-lg absolute left-1/2 -translate-x-1/2 top-1/4">Preview Loading...</p>}
+                    <video 
+                    onLoadedData={() => setIsLoaded(true)}
+                    src={project.video}
+                    controls
+                    autoPlay 
+                    loop 
+                    muted 
+                    playsInline 
+                    ></video>
                     </div>
                     <h3 className="text-lg font-bold text-left md:mx-8 mt-8 mb-4">Description:</h3>
                     {descriptionParagraphs}
