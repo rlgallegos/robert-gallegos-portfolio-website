3f056b527d7ea398fc83eb6442bdd601c47d9c75
Robert <rlgallegos85@gmail.com>
2023-09-07 18:12:59 -0400

    Finished updating data

--- src/data.js
+++ src/data.js
@@ -60,9 +60,9 @@ export const projects = [
       "id": 1,
       "title": "Skyvault",
       "description": [
-        "Restaurant Companion is a full-stack, mobile-responsive application developed as a capstone project at Flatiron. Its intended use is to smooth out two of the most difficult aspects of server-guest communication in the restaurant industry: language, and allergies. From the guest perspective, a QR code is given to them directly leading them to the restaurant companion for the given restaurant. On scanning, the user can tap filter through the allergies the restaurant has on its menu, as well as translate the dish and its allergies, building the order as they go in whatever language(s) they choose. Once they are finished, the order is translated into english, allowing them to see both the translated and untranslated version of the order, and assigning them a number. From here, they can show the order to the server, or, optionally, give the designated order number to the  server who can, themselves, pull up the english version of the order via a hidden route.",
-        "From the managerial perspective, the manager creates a restaurant, and before being allowed to navigate freely, is required to sign up for a 14-day free trial on Stripe. They can create new users, give them specific roles, build a menu with its specific dishes and allergies, and manage their account settings including payment details with Stripe, etc, from inside the 'Manager Portal'.",
-        "Developing the Restaurant Companion required the use of several different languages/frameworks. Frontend was React, leveraging its component structure to split the website into the two 'portals.' Styling was accomplished using TailwindCSS. Flask was the web framework chosen; using RESTFUL routes where logical and custom routes where necessary. The backend language was Python which allowed integration with Google Translate's library (googletrans). An online database (PostgreSQL) was used, with flask-migrate handling all migrations. Authorization and authentication were handled by 'bcrypt.'"
+        "Skyvault is an ongoing project designed to market the Skyvault app, which is currently available for both iOS and Android. The primary goal of the app is to assist small business owners in efficiently managing their inventory, keeping track of all associated costs and profits related to inventory management.", 
+        "The website itself is a single-page application developed using React.js, and it plays a pivotal role in promoting the Skyvault app. Throughout the development process, one of the most significant challenges was ensuring mobile responsiveness. Given that the target audience primarily uses their mobile devices for business purposes, meticulous attention was dedicated to crafting an exceptional UI/UX design optimized for smaller screens.",
+        "Presently, our focus is centered on integrating the website with the mobile application seamlessly. Additionally, we are in the process of establishing an API that will facilitate automatic email sending, enhancing the overall user experience and functionality of both the app and the website."
       ],
       "image": "skyvault-preview-pic.jpeg",
       "git": "https://github.com/rlgallegos/skyvault-website",
@@ -76,7 +76,10 @@ export const projects = [
       ],
       "skills": [
         "User-driven UI/UX",
-        "Mobile-responsive design"
+        "Mobile-responsive design",
+        "Mobile App Development",
+        "UI/UX Design",
+        "API Integration",
       ]
     },
     {
@@ -94,7 +97,8 @@ export const projects = [
       "languages": [
         "Python",
         "JavaScript (React)",
-        "SQLAlchemy"
+        "SQLAlchemy",
+        "JSON"
       ],
       "skills": [
         "Backend/Frontend Deployment",
@@ -114,10 +118,17 @@ export const projects = [
       "demo": "https://space-blasters.onrender.com/",
       "video": "space-blasters-demo-video.mp4",
       "languages": [
-        "JavaScript (React)"
+        "JavaScript (React)",
+        "HTML/CSS",
+        "JSON"
       ],
       "skills": [
-        "Express (JavaScript)"
+        "React.js",
+        "Frontend Validation",
+        "Frontend Routing",
+        "CRUD Operations",
+        "Express",
+        "JSON-Server"
       ]
     }
   ]
